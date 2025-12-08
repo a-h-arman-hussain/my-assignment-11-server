9f5cbdd572d6f4a314cd4b450bfd40df6c7d4913
a-h-arman-hussain <armanhd16@gmail.com>
2025-12-08 14:38:03 +0600

    added admin, moderator, students apis

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
+const { chownSync } = require("fs");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const app = express();
 const port = 5000;
@@ -53,8 +54,60 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users", async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.patch("/users/:id/role", async (req, res) => {
+      const id = req.params.id;
+      const { role } = req.body;
+      const result = await usersCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { role } }
+      );
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await usersCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    app.post("/add-scholarship", async (req, res) => {
+      try {
+        const scholarship = req.body;
+        const result = await scholarCollection.insertOne(scholarship);
+        res.send({ success: true, insertedId: result.insertedId });
+      } catch (err) {
+        console.error("Add Scholarship Error:", err);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
+      }
+    });
+
     app.get("/all-scholarships", async (req, res) => {
       const result = await scholarCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch("/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      const result = await scholarCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      );
+      res.send(result);
+    });
+
+    app.delete("/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await scholarCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
       res.send(result);
     });
 
@@ -84,36 +137,25 @@ async function run() {
       try {
         const application = req.body;
 
-        // prevent duplicate application
+        // application.studentEmail = application.studentEmail; // add this line
+        console.log(application);
+
         const exists = await applicationsCollection.findOne({
           scholarshipId: application.scholarshipId,
-          userEmail: application.userEmail,
+          studentEmail: application.studentEmail,
         });
 
         if (exists) {
           return res.send({ success: false, message: "Already applied!" });
         }
 
-        // insert new application
         const result = await applicationsCollection.insertOne(application);
 
         res.send({ success: true, insertedId: result.insertedId });
       } catch (err) {
-        console.error("Apply Error:", err);
+        console.error(err);
         res.status(500).send({ success: false, message: "Server error" });
       }
-    });
-
-    app.get("/my-applications", async (req, res) => {
-      const email = req.query.email;
-      if (!email) return res.status(400).send([]); // 400 Bad Request
-
-      const result = await applicationsCollection
-        .find({ studentEmail: email })
-        .sort({ appliedAt: -1 })
-        .toArray();
-
-      res.send(result);
     });
 
     // app.post("/apply-scholarships", async (req, res) => {
@@ -130,19 +172,227 @@ async function run() {
     //       return res.send({ success: false, message: "Already applied!" });
     //     }
 
+    //     // insert new application
     //     const result = await applicationsCollection.insertOne(application);
 
     //     res.send({ success: true, insertedId: result.insertedId });
     //   } catch (err) {
-    //     console.error(err);
+    //     console.error("Apply Error:", err);
     //     res.status(500).send({ success: false, message: "Server error" });
     //   }
     // });
+
+    app.get("/my-applications", async (req, res) => {
+      const email = req.query.email;
+      if (!email) return res.status(400).send([]);
+
+      const result = await applicationsCollection
+        .find({ studentEmail: email })
+        .sort({ appliedAt: -1 })
+        .toArray();
+
+      res.send(result);
+    });
+
+    app.get("/my-applications/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const result = await applicationsCollection.findOne({
+          _id: new ObjectId(id),
+        });
+
+        if (!result)
+          return res.status(404).send({ message: "Application not found" });
+
+        res.send(result);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Server error" });
+      }
+    });
+
+    app.patch("/update-application/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const body = req.body;
+
+        const updateData = {
+          ...body,
+          address: body.address,
+          phone: body.phone,
+          previousEducation: body.previousEducation,
+        };
+
+        const update = await applicationsCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updateData }
+        );
+        res
+          .status(200)
+          .json({ success: true, modifiedCount: update.modifiedCount });
+      } catch (error) {
+        console.log(error);
+        res
+          .status(500)
+          .json({ success: false, message: "Internal server error" });
+      }
+    });
+
+    app.delete("/delete-application/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await applicationsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    // get /applications/:id for moderator
+    app.get("/applications", async (req, res) => {
+      try {
+        const applications = await applicationsCollection
+          .find()
+          .sort({ appliedAt: -1 })
+          .toArray();
+        res.send(applications);
+      } catch (err) {
+        console.error(err);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
+      }
+    });
+
+    // PATCH /applications/:id for moderator
+    app.patch("/applications/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const { status } = req.body;
+
+        const result = await applicationsCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { applicationStatus: status } }
+        );
+
+        res.send({ success: true, modifiedCount: result.modifiedCount });
+      } catch (err) {
+        console.error(err);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
+      }
+    });
+
+    app.post("/add-review", async (req, res) => {
+      try {
+        const review = req.body;
+        const { studentEmail, scholarshipId } = review;
+        const existingReview = await reviewsCollection.findOne({
+          studentEmail,
+          scholarshipId,
+        });
+
+        if (existingReview) {
+          return res.status(400).send({
+            success: false,
+            message: "You have already submitted a review for this scholarship",
+          });
+        }
+        const result = await reviewsCollection.insertOne(review);
+        res.send({ success: true, reviewId: result.insertedId });
+      } catch (err) {
+        console.error("Add review error:", err);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
+      }
+    });
+
+    app.get("/reviews", async (req, res) => {
+      try {
+        const reviews = await reviewsCollection.find().toArray();
+        res.send(reviews);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Internal server error" });
+      }
+    });
+
+    app.delete("/reviews/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const result = await reviewsCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+
+        res.send(result);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Internal server error" });
+      }
+    });
+
+    app.get("/my-reviews", async (req, res) => {
+      try {
+        const email = req.query.email;
+        if (!email)
+          return res
+            .status(400)
+            .send({ success: false, message: "Email required" });
+
+        const reviews = await reviewsCollection
+          .find({ studentEmail: email })
+          .sort({ reviewDate: -1 })
+          .toArray();
+
+        res.send(reviews);
+      } catch (err) {
+        console.error("Get reviews error:", err);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
+      }
+    });
+
+    app.patch("/update-review/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const updateData = req.body; // { rating, comment }
+
+        const result = await reviewsCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updateData }
+        );
+
+        res.send({ success: true, modifiedCount: result.modifiedCount });
+      } catch (err) {
+        console.error("Update review error:", err);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
+      }
+    });
+
+    app.delete("/delete-review/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const result = await reviewsCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+        res.send({ success: true, deletedCount: result.deletedCount });
+      } catch (err) {
+        console.error("Delete review error:", err);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
+      }
+    });
 
     // payment related apis
     // new
     app.post("/payment-checkout-session", async (req, res) => {
       const paymentInfo = req.body;
+      console.log("paymentInfo", paymentInfo);
       const amount = Number(paymentInfo.applicationFees) * 100;
       const session = await stripe.checkout.sessions.create({
         line_items: [
@@ -170,43 +420,102 @@ async function run() {
     });
 
     app.patch("/payment-success", async (req, res) => {
-      const sessionId = req.query.session_id;
-      const session = await stripe.checkout.sessions.retrieve(sessionId);
-      console.log("session", session);
-      if (session.payment_status === "paid") {
-        const id = session.metadata.scholarshipId;
-        const query = { _id: new ObjectId(id) };
-        const update = {
-          $set: {
-            paymentStatus: "paid",
-            trackingId: generateTrackingId(),
+      try {
+        const sessionId = req.query.session_id;
+        const session = await stripe.checkout.sessions.retrieve(sessionId);
+        // console.log(session);
+
+        if (session.payment_status !== "paid") {
+          return res.status(400).send({ success: false });
+        }
+
+        const scholarshipId = session.metadata.scholarshipId;
+        const trackingId = generateTrackingId();
+
+        // use the correct field
+        const userEmail = session.customer_email;
+        // console.log(scholarshipId);
+
+        const updateResult = await applicationsCollection.updateOne(
+          {
+            scholarshipId: scholarshipId,
+            // studentEmail: userEmail,
           },
-        };
-        const result = await applicationsCollection.updateOne(query, update);
+          {
+            $set: {
+              paymentStatus: "paid",
+              trackingId: trackingId,
+            },
+          }
+        );
+        // console.log(updateResult);
 
         const paymentHistory = {
           amount: session.amount_total / 100,
           currency: session.currency,
-          customerEmail: session.customer_email,
+          customerEmail: userEmail,
           scholarshipId: session.metadata.scholarshipId,
           scholarshipName: session.metadata.scholarshipName,
           transactionId: session.payment_intent,
           paymentStatus: session.payment_status,
           paidAt: new Date(),
         };
-        if (session.payment_status === "paid") {
-          const resultPayment = await paymentCollection.insertOne(
-            paymentHistory
-          );
-          res.send({
-            success: true,
-            modifyScholar: result,
-            paymentInfo: resultPayment,
-          });
-        }
-        // res.send(result);
+
+        // const insertPayment = await paymentCollection.insertOne(paymentHistory);
+
+        res.send({
+          success: true,
+          //   updatedApplication: updateResult,
+          trackingId,
+          transactionId: session.payment_intent,
+          //   paymentInfo: insertPayment,
+        });
+      } catch (err) {
+        console.error("Payment success error:", err);
+        res.status(500).send({ error: "Internal server error" });
       }
     });
+
+    // app.patch("/payment-success", async (req, res) => {
+    //   const sessionId = req.query.session_id;
+    //   const session = await stripe.checkout.sessions.retrieve(sessionId);
+    //   console.log("session", session);
+    //   if (session.payment_status === "paid") {
+    //     const id = session.metadata.scholarshipId;
+    //     const query = { _id: new ObjectId(id) };
+    //     const update = {
+    //       $set: {
+    //         paymentStatus: "paid",
+    //         trackingId: generateTrackingId(),
+    //       },
+    //     };
+    //     const result = await applicationsCollection.updateOne(query, update);
+
+    //     const paymentHistory = {
+    //       amount: session.amount_total / 100,
+    //       currency: session.currency,
+    //       customerEmail: session.customer_email,
+    //       scholarshipId: session.metadata.scholarshipId,
+    //       scholarshipName: session.metadata.scholarshipName,
+    //       transactionId: session.payment_intent,
+    //       paymentStatus: session.payment_status,
+    //       paidAt: new Date(),
+    //     };
+    //     if (session.payment_status === "paid") {
+    //       const resultPayment = await paymentCollection.insertOne(
+    //         paymentHistory
+    //       );
+    //       res.send({
+    //         success: true,
+    //         modifyScholar: result,
+    //         trackingId: trackingId,
+    //         transactionId: session.payment_intent,
+    //         paymentInfo: resultPayment,
+    //       });
+    //     }
+    //     // res.send(result);
+    //   }
+    // });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
