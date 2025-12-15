c58c67178a73d7986aa76a1c5cd9025f3d4a62d5
a-h-arman-hussain <armanhd16@gmail.com>
2025-12-15 15:08:54 +0600

    added service key

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceKey.json");
+// const serviceAccount = require("./serviceKey.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
